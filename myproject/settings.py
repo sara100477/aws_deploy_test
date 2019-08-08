27ba677b5dfe8b0e690554648cfb113da029b6fb
Seran <sara100477@gmail.com>
2019-08-08 18:27:00 +0900

    last

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&qrv(m*)8=_1oa38us(h@!g618*@fvd3@8#xnwe8gu(4@#jrx$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aws-env.xccbzmvfpk.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['aws-env.xccbzmvfpk.us-west-2.elasticbeanstalk.com','*']
 
 
 # Application definition
